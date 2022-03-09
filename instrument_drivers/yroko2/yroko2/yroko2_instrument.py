8d682140c1df108d31db66be3037ea6beca689b0
Evan McKinney <evmckinney9@gmail.com>
2022-03-09 16:58:36 -0500

    add yroko2.0 init commit

---
+++ instrument_drivers/yroko2/yroko2/yroko2_instrument.py
@@ -0,0 +1,3 @@
+# TODO: instantiate qccodes.instrument abstract class
+# TODO: use info from original YROKO_Client.py to make work like other drivers in the lab
+# TOOD: python multithread, mutex yroko
